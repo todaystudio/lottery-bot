e639bbf63d380356cdd2d78835ee56df63973cc0
todaystudio <58150891+todaystudio@users.noreply.github.com>
2023-04-05 22:40:20 +0300

    05-04 add controller hello

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from "@nestjs/common";
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('aa')
-  getHello(): string {
-    return 'Hello'
+  @Get('add-ticket/:tgId')
+  getHello(@Param('tgId') id: string) {
+    return id.toString()
   }
 }
